4c9dc96727dff99f6e99c50a2de02fef0d9a24dd
Daniel Olmedo <da.olmedon@gmail.com>
2015-11-29 19:47:01 +0100

    CPUTileStorage v1.0

--- Proland/Core/Producer/CPUTileStorage.cs
+++ Proland/Core/Producer/CPUTileStorage.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using log4net;
+using Sxta.Core;
+using Sxta.Math;
+using Sxta.Render;
+using Sxta.Render.Scenegraph;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Sxta.Proland.Core.Producer
 {
@@ -31,7 +35,7 @@ namespace Sxta.Proland.Core.Producer
 82 	    * @param size the number of elements in the data array.
 83 	    */
 
-        public CPUSlot(TileStorage owner, int size)
+        public CPUSlot(TileStorage owner, int size) : base(owner) 
         {
             this.data = new T.size; //new T[size]
             this.size = size;
@@ -40,10 +44,86 @@ namespace Sxta.Proland.Core.Producer
         ~CPUSlot()
         {
             if (data != null)
-                {
-                    delete[] data;  //Delete process?????
-                }
+            {
+                delete[] data;  //Delete process?????
+            }
         }
 
+        /** ------------------------------------CONSTRUCTOR------------------------------------ **
+        * Creates a new CPUTileStorage.
+        *
+        * @param tileSize the size in pixels of each (square) tile.
+        * @param channels the number of components per pixel of each tile. Each
+        *      component is of type T.
+        * @param capacity the number of slots managed by this tile storage.
+        */
+        CPUTileStorage(int tileSize, int channels, int capacity) : base(owner)
+        {
+            init(tileSize, channels, capacity);
+        }
+
+        /**
+        * Deletes this CPUTileStorage.
+        */
+        virtual ~CPUTileStorage()
+        {
+            //      ¿EMPTY?
+        }
+        /**
+        * Returns the number of components per pixel of each tile. Each component
+        * is of type T.
+        */
+        int getChannels()
+        {
+            return channels;
+        }
+        /**
+        * Creates an uninitialized CPUTileStorage.
+        */
+        protected CPUTileStorage() : TileStorage()
+        {
+            //      ¿EMPTY?
+        }
+        /**
+        * Initializes this CPUTileStorage.
+        *
+        * @param tileSize the size in pixels of each (square) tile.
+        * @param channels the number of components per pixel of each tile. Each
+        *      component is of type T.
+        * @param capacity the number of slots managed by this tile storage.
+        */
+        protected void init(in tileSize, int channels, int capacity)
+        {
+            TileStorage.init(tileSize, capacity);
+            this.channels = channels;
+            int size = tileSize * tileSize * channels;
+            for (int i = 0; i < capacity; i++)
+            {
+                freeSlots.push_back(new CPUSlot(this, size));
+            }
+        }
+
+        protected void swap(ptr<CPUTileStorage<T>> t)
+        {
+            //  ¿EMPTY?
+        }
+
+        /**
+        * The number of components per pixel of each tile.
+        */
+        private int channels;
+
+        extern const char cpuByteTileStorage[] = "cpuByteTileStorage";
+
+        extern const char cpuFloatTileStorage[] = "cpuFloatTileStorage";
+
+        static ResourceFactory::Type<cpuByteTileStorage, CPUTileStorageResource<unsigned char>> CPUByteTileStorageType;
+
+        static ResourceFactory::Type<cpuFloatTileStorage, CPUTileStorageResource<float>> CPUFloatTileStorageType;
+
+        public CPUTileStorageResource() : ResourceTemplate<0, CPUTileStorage<T>>()
+ 	    {
+
+ 	    }
     }
 }
