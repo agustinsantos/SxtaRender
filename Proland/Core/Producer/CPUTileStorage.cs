72a06fc4fb8aff931ad06fa63119125da1ac942a
Daniel Olmedo <da.olmedon@gmail.com>
2015-12-23 20:07:49 +0100

    TileStorage Commit Conflict

--- Proland/Core/Producer/CPUTileStorage.cs
+++ Proland/Core/Producer/CPUTileStorage.cs
@@ -10,31 +10,31 @@ using System.Reflection;
 
 namespace Sxta.Proland.Core.Producer
 {
-#if TODO
+//#if TODO
     /**
-51 	 * A TileStorage that store tiles on CPU.
-52 	 * @ingroup producer
-53 	 * @authors Eric Bruneton, Antoine Begault
-54 	 *
-55 	 * @tparam T the type of each tile pixel component (e.g. char, float, etc).
-56 	 */
+ 	 * A TileStorage that store tiles on CPU.
+ 	 * @ingroup producer
+ 	 * @authors Eric Bruneton, Antoine Begault
+ 	 *
+ 	 * @tparam T the type of each tile pixel component (e.g. char, float, etc).
+ 	 */
     public class CPUTileStorage<T> : TileStorage
     {
         /**
-68 	    * The data of the tile stored in this slot.
-69 	    */
+ 	    * The data of the tile stored in this slot.
+ 	    */
         public T data;
         /**
-73 	    * The number of elements in the data array.
-74 	    */
+ 	    * The number of elements in the data array.
+ 	    */
         public int size;
         /**
-78 	    * Creates a new CPUSlot. This constructor creates a new array to store
-79 	    * the tile data.
-80 	    *
-81 	    * @param owner the TileStorage that manages this slot.
-82 	    * @param size the number of elements in the data array.
-83 	    */
+ 	    * Creates a new CPUSlot. This constructor creates a new array to store
+ 	    * the tile data.
+ 	    *
+ 	    * @param owner the TileStorage that manages this slot.
+ 	    * @param size the number of elements in the data array.
+ 	    */
 
         public CPUSlot(TileStorage owner, int size) : base(owner) 
         {
